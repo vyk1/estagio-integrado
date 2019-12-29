ed164f29ac9011c468b97185e3b23f2a13c0f571
vyk1 <victoriabotelho14@gmail.com>
2019-12-29 00:50:32 -0300

    servidor correto

--- ei/src/config/server.js
+++ ei/src/config/server.js
@@ -1,2 +1,2 @@
-module.exports= 'http://192.168.0.14:21089'
-// module.exports= 'http://estagiointegrado.kinghost.net:21089'
+// module.exports= 'http://192.168.0.14:21089'
+module.exports= 'http://estagiointegrado.kinghost.net:21089'
