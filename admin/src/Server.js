dd89c22ac8c1603dc77b11e0498fd7f50fab9342
vyk1 <victoriabotelho14@gmail.com>
2020-10-22 21:37:07 -0300

    Fix: backend

--- admin/src/Server.js
+++ admin/src/Server.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://estagiointegrado.kinghost.net:21089",
+    baseURL: "https://eiback.azurewebsites.net",
     // baseURL: "http://192.168.0.105:21089",
 });
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+const app = express()
 
-const server = require('http').Server(app);
+const server = require('http').Server(app)
 try {
-  mongoose.connect('mongodb://estagiointegra01:developer2020@mongo71-farm76.kinghost.net/estagiointegra01', {
-  // mongoose.connect('mongodb+srv://developer:developer@cluster0-dqw7t.mongodb.net/eiDB', {
+  // mongoose.connect('mongodb://estagiointegra01:developer2020@mongo71-farm76.kinghost.net/estagiointegra01', {
+  mongoose.connect('mongodb+srv://developer:developer@cluster0-dqw7t.mongodb.net/eiDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
   })
   mongoose.connection.on('connected', function () {
-    console.log('Conectou');
-  });
+    console.log('Conectou')
+  })
 
 } catch (error) {
-  console.log(error);
+  console.log(error)
   throw new Error('Erro na Conexão com o Banco de Dados.')
 }
 
-app.use(cors());
+app.use(cors())
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.use(require('./routes'))
 
-server.listen(21089);
+server.listen(process.env.PORT)
